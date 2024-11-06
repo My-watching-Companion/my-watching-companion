c7dfc98625ecea1b832fa6446d112b42fb141b5f
svasco <76670937+semanavasco@users.noreply.github.com>
2024-11-06 18:39:06 +0100

    Added sidebar
    Made the website responsive by adding a sidebar toggle when the screen size becomes smaller than 900px.

---
+++ public/scripts/main.js
@@ -0,0 +1,9 @@
+function openSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.style.display = "flex";
+}
+
+function closeSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.style.display = "none";
+}
