d9ab5827312f4d6ff15da5c2c6c330330579ad05
svasco <76670937+semanavasco@users.noreply.github.com>
2025-03-06 17:32:18 +0100

    Added SQL injection fix

--- db.js
+++ db.js
@@ -1,34 +1,36 @@
-const sql = require("mssql")
-const {USER, PASSWORD, SERVER, DATABASE} = require("./config")
+const sql = require("mssql");
+const { USER, PASSWORD, SERVER, DATABASE } = require("./config");
 
 const config = {
-    user: USER, 
-    password: PASSWORD, 
-    server: SERVER,
-    database: DATABASE,
-    options: {
-        encrypt: false, 
-        trustServerCertificate: true 
-    }
+  user: USER,
+  password: PASSWORD,
+  server: SERVER,
+  database: DATABASE,
+  options: {
+    encrypt: false,
+    trustServerCertificate: true,
+  },
 };
 
 const executeQuery = async (query, params = []) => {
-    try {
-      
-      const pool = await sql.connect(config);
-      const request = pool.request();
-  
-      
-      params.forEach((param, index) => {
-        request.input(`param${index + 1}`, param); 
-      });
-  
-      
-      const result = await request.query(query);
-      return result.recordset; 
-    } catch (err) {
-      throw err; 
-    }
-  };
-  
-  module.exports = { executeQuery };
+  try {
+    const pool = await sql.connect(config);
+    const request = pool.request();
+
+    params.forEach((param) => {
+      if (!param.name || !param.type || param.value === undefined) {
+        throw new Error(
+          "Each parameter must have name, type and value properties"
+        );
+      }
+      request.input(param.name, param.type, param.value);
+    });
+
+    const result = await request.query(query);
+    return result.recordset;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { executeQuery, sql };
